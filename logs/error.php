74d3eb90168d5829a539d1f7a322a2ca9aafddd7
lpesantesm <lpesantesm@gmail.com>
2016-04-02 07:58:04 -0500

    proyecto

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-03-30 15:21:06 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-03-30T15:21:06+00:00	INFO ::1	joomlafailure	No se permiten contraseñas vacías
+2016-04-02T04:24:38+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
